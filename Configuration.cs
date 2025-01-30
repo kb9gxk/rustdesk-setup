7154ca8ec267743539d67f60b8528065fc086d12
Jeff Parrish <8zqcce0t@duck.com>
2025-01-30 11:10:41 -0600

    Update Configuration.cs

--- Configuration.cs
+++ Configuration.cs
@@ -3,11 +3,12 @@
 namespace RustdeskSetup
 {
     internal static class Configuration
-    {
-        private const string DefaultConfigValue = "someConfigValue";
-        private const string DefaultPasswordValue = "somePassword";
-        internal static bool? useStableVersion = null;
-        internal static string rustdeskCfg = DefaultConfigValue;
-        internal static string rustdeskPw = DefaultPasswordValue;
-    }
+{
+    private const string DefaultConfigValue = "";
+    private const string DefaultPasswordValue = "";
+
+    internal static bool? UseStableVersion { get; set; }
+    internal static string RustdeskCfg { get; set; } = DefaultConfigValue;
+    internal static string RustdeskPw { get; set; } = DefaultPasswordValue;
+}
 }
