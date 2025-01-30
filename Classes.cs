c598208e0622a2ae89f77c981c112cc8abfd1d3f
Jeff Parrish <8zqcce0t@duck.com>
2025-01-30 10:15:55 -0600

    Update Classes.cs

--- Classes.cs
+++ Classes.cs
@@ -1,11 +1,21 @@
-﻿public class Asset
-{
-    public string name { get; set; }
-    public string browser_download_url { get; set; }
-}
+using System.Text.Json.Serialization;
 
-public class GitHubRelease
+namespace RustdeskSetup
 {
-    public string tag_name { get; set; }
-    public Asset[] assets { get; set; }
+    public class Asset
+    {
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+        [JsonPropertyName("browser_download_url")]
+        public string browser_download_url { get; set; }
+    }
+
+    [JsonSerializable(typeof(GitHubRelease))]
+    public partial class GitHubRelease
+    {
+        [JsonPropertyName("tag_name")]
+        public string tag_name { get; set; }
+        [JsonPropertyName("assets")]
+        public Asset[] assets { get; set; }
+    }
 }
