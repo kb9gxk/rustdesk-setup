cf3ce0ddf2a83a35f131f81268de3ec7c09c8f7c
Jeff Parrish <8zqcce0t@duck.com>
2025-01-30 10:28:52 -0600

    Update NativeMethods.cs

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -1,25 +1,23 @@
 using System;
 using System.Runtime.InteropServices;
+using System.Security;
 
 namespace RustdeskSetup
 {
     internal static class NativeMethods
     {
         // Declaration for MessageBox function from user32.dll
-        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        [DllImport("user32.dll", CharSet = CharSet.Unicode), SuppressUnmanagedCodeSecurity]
         internal static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
 
         // Add other DllImport declarations here for other native functions as needed
         internal const int SW_HIDE = 0;
         internal const int SW_SHOW = 5;
 
-        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        [DllImport("kernel32.dll"), SuppressUnmanagedCodeSecurity]
         internal static extern IntPtr GetConsoleWindow();
 
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        [DllImport("user32.dll"), SuppressUnmanagedCodeSecurity]
         internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
     }
-
-
-
 }
