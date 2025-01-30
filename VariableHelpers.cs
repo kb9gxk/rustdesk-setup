847d3fda8e3f53f171fed18d6b9ae4405f3afe21
Jeff Parrish <8zqcce0t@duck.com>
2025-01-30 13:51:25 -0600

    Update VariableHelpers.cs

--- VariableHelpers.cs
+++ VariableHelpers.cs
@@ -1,4 +1,3 @@
-// VariableHelpers.cs
 using System.Text.Json.Serialization;
 
 namespace RustdeskSetup
@@ -10,16 +9,16 @@ namespace RustdeskSetup
     public class GitHubRelease
     {
         [JsonPropertyName("tag_name")]
-        public string tag_name { get; set; }
+        public string? tag_name { get; set; }
         [JsonPropertyName("assets")]
-        public Asset[] assets { get; set; }
+        public Asset[]? assets { get; set; }
     }
 
     public class Asset
     {
         [JsonPropertyName("name")]
-        public string name { get; set; }
+        public string? name { get; set; }
         [JsonPropertyName("browser_download_url")]
-        public string browser_download_url { get; set; }
+        public string? browser_download_url { get; set; }
     }
 }
