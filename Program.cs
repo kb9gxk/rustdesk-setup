e49fbe075fa702260fec14b9b1353c352a679048
Jeff Parrish <8zqcce0t@duck.com>
2025-01-30 10:45:42 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -64,37 +64,37 @@ namespace RustdeskSetup
         static async Task InitializeAsync(bool useStableVersion)
         {
             InstallationSettings.HideWindow();
-
+        
             string editionString = useStableVersion ? "Stable" : "Nightly";
             string githubApiUrl = useStableVersion ? InstallationSettings.githubStableApiUrl : InstallationSettings.githubNightlyApiUrl;
             InstallationSettings.editionString = editionString;
             InstallationSettings.logFilePath = $"c:\\Rustdesk-{editionString}-Install.log";
-
+        
             (string rustdeskUrl, string version) = await GitHubHelper.GetLatestRustdeskInfoAsync(githubApiUrl);
-
+        
             if (string.IsNullOrEmpty(rustdeskUrl) || string.IsNullOrEmpty(version))
             {
                 InstallationSettings.log?.WriteLine($"Failed to retrieve {editionString} Rustdesk information. Exiting.");
                 return;
             }
-
+        
             InstallationSettings.log?.WriteLine($"Using Rustdesk {editionString} version: {version}");
             InstallationSettings.log?.WriteLine($"Using Rustdesk {editionString} URL: {rustdeskUrl}");
-
-            Installation.DownloadAndInstallRustdesk(rustdeskUrl, InstallationSettings.tempDir);
-
+        
+            await Installation.DownloadAndInstallRustdeskAsync(rustdeskUrl, InstallationSettings.tempDir); // Await the download
+        
             string rustdeskDir = Installation.GetRustdeskDirectory();
             string runMe = Installation.GetRustdeskExecutable(rustdeskDir);
             string rustdeskId = Installation.GetRustdeskId(runMe, rustdeskDir);
-
+        
             if (!string.IsNullOrEmpty(Configuration.rustdeskCfg))
             {
                 Utility.ConfigureAndRunRustdesk(rustdeskId, runMe, Configuration.rustdeskCfg, Configuration.rustdeskPw);
             }
-
+        
             Utility.SaveRustdeskInfo(rustdeskId);
             Utility.DisplayPopup(rustdeskId, version);
-
+        
             Installation.Cleanup(InstallationSettings.tempDir, InstallationSettings.rustdeskExe);
         }
     }
